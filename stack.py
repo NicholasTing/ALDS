c5e0f862e805abacf39f1c0020a100dfcecd1a0e
Nicholas Ting <nicholasting92@gmail.com>
2018-01-11 10:24:30 +0800

    Create stack.py
    It isnt complete, still fixing some errors.

---
+++ stack.py
@@ -0,0 +1,37 @@
+#Coded by Nicholas Ting
+#Coded on 11/1/2018
+#Revision on Python 3
+#Stack coded in python3
+#Incomplete
+
+num_elems = int(input())
+
+print("Please input %d number of elements to be inserted." % num_elems)
+
+stack = []
+
+print("Input %d elements one at a time" % num_elems)
+
+while num_elems != 0:
+    element = int(input())
+    stack.append(element)
+    num_elems -= 1
+
+print("Starting stack: ")
+print(stack)
+
+print("What would you like to do?")
+print("Commands: append pop exit")
+
+command = input()
+
+while command != "exit":
+
+    if command == "append":
+        print("Initial stack")
+        print(stack)
+        print("Input the element you would like to append")
+        elem = int(input())
+        stack.append(elem)
+        print("Updated stack")
+        print(stack)
